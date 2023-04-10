ea707eee58ce8f9214926a794947955daffe5240
Spyabo <reesewright02@protonmail.com>
2023-04-10 13:39:04 +0100

    Make max scoreboard entries 15

--- src/components/TopScores.jsx
+++ src/components/TopScores.jsx
@@ -2,17 +2,21 @@ import { useEffect } from "react";
 
 const TopScores = ({ highScores }) => {
   useEffect(() => {
+    if (highScores.length > 15) {
+      highScores.length = 15;
+    }
     localStorage.setItem("highScores", JSON.stringify(highScores));
   }, [highScores]);
 
   return (
-    <div>
+    <div id="TopScores">
       {highScores.length > 0
         ? highScores
             .sort((a, b) => b[1] - a[1])
             .map((score, index) => {
               return <p key={index}>{`${score[0]} - ${score[1]}`}</p>;
             })
+            .slice(0, 15)
         : undefined}
     </div>
   );
